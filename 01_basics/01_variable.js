00c8c82010a89309089d0f32b7770237ca1c4ce1
manastendulkar10 <134383425+manastendulkar10@users.noreply.github.com>
2024-09-16 18:42:41 +0000

    let , var , const

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+  const accountId = 1010
+  let accountEmail = "manas@google.com"
+  var accountPassword = "Manas"
+  accountCity = "Pune"
+  let accountstate 
+
+// accountId = 2 // not allowed
+
+/*
+prefer  not to use var
+*/
+  
+  accountEmail = "tendulkar@google.com"
+  accountPassword ="123456"
+  accountCity = "Kolhapur"
+
+
+  console.log(accountId);
+
+  console.table([accountId,accountEmail,accountPassword,accountstate,accountCity])
